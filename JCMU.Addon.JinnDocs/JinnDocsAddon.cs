1d05197340a499cf7139831eaae0630e5d3f2273
Steven Fletcher <steven@jinnstudios.com>
2026-06-03 14:33:48 -0500

    Initial Commit.

---
+++ JCMU.Addon.JinnDocs/JinnDocsAddon.cs
@@ -0,0 +1,13 @@
+﻿using JinnDev.JCMU.SDK.Interfaces;
+using JinnDev.JCMU.SDK.Models;
+using JinnDev.Utilities.Monad;
+
+namespace JinnDev.JCMU.Addon.JinnDocs;
+
+public class JinnDocsAddon : IJcmuAddon
+{
+    public async Task<Maybe<int>> ExecuteAsync(ActionContext context)
+    {
+        return Maybe.Some(-1);
+    }
+}
